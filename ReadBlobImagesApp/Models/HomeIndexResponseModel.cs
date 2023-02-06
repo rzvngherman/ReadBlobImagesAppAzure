ac6b1c56f3db1da1388cc709633b32b960172df4
Razvan Gherman <r.gherman@levi9.com>
2023-02-06 16:47:13 +0200

    First commit.

---
+++ ReadBlobImagesApp/Models/HomeIndexResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ReadBlobImagesApp.Models
+{
+    public class HomeIndexResponseModel : Microsoft.AspNetCore.Mvc.RazorPages.PageModel
+    {
+        public string ContainerName { get; set; }
+        public List<string> Urls { get; set; }
+    }
+}
